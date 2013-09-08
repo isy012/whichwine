83b002c4ae0e7cfe4b770b0c30d03d88ca6dbc7c
Isabelle Park <izzy@chalo.io>
2013-09-07 22:50:57 -0400

    Tricking heroku

---
+++ db/migrate/20130908024736_create_users_table.rb
@@ -0,0 +1,15 @@
+class CreateUsersTable < ActiveRecord::Migration
+  def change
+    create_table :users_tables do |t|
+    	t.string :name
+      	t.string :email
+      	t.string :provider
+    	t.string :uid
+    	t.string :name
+    	t.string :oauth_token
+    	t.datetime :oauth_expires_at
+
+      	t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130908020049) do
+ActiveRecord::Schema.define(version: 20130908024736) do
 
   create_table "users", force: true do |t|
     t.string   "email"
